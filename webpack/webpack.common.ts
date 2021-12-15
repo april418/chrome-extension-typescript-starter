6b2d946cf0931ec949f39b86514cb40cfc9efd1f
april418 <apri.4.18@gmail.com>
2021-12-15 02:18:35 +0000

    update

---
+++ webpack/webpack.common.ts
@@ -0,0 +1,35 @@
+import path from 'path'
+import CopyPlugin from 'copy-webpack-plugin'
+
+const srcDir = path.join(__dirname, '..', 'src')
+const config = {
+  entry: {
+    popup: path.join(srcDir, 'popup.tsx'),
+    options: path.join(srcDir, 'options.tsx'),
+    background: path.join(srcDir, 'background.ts'),
+    content_script: path.join(srcDir, 'content_script.tsx'),
+  },
+  output: {
+    path: path.join(__dirname, '../dist/js'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: '.', to: '../', context: 'public' }],
+    }),
+  ],
+}
+
+export default config
